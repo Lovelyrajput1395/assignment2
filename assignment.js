a54a6f0200fc2728df76ba921517f6b2252c0e32
Lovelyrajput1395 <157692801+Lovelyrajput1395@users.noreply.github.com>
2024-02-27 17:20:51 +0530

    assignment.js added

---
+++ assignment.js
@@ -0,0 +1,256 @@
+
+import "./assign.css"
+import { FcApproval } from "react-icons/fc";
+import { IoIosInformationCircle } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosStar } from "react-icons/io";
+import { IoIosStarHalf } from "react-icons/io";
+import { IoLogoCodepen } from "react-icons/io";
+import { IoIosCheckmarkCircleOutline } from "react-icons/io";
+import { IoIosSearch } from "react-icons/io";
+
+
+
+function App() {
+    return (
+        <div>
+            <div>
+                <header>
+                    <div>
+                        <input type="search"></input>
+                        <p className="search"><IoIosSearch /></p>
+                    </div>
+                    <div>
+                        <ul>
+
+                            <li>Categories</li>
+                            <li>Website Builders</li>
+                            <li>Today's deal</li>
+                        </ul>
+                    </div>
+                </header>
+
+                <div className="center">
+                    <h1>Best Website builders in the US</h1>
+                    <div className="content1">
+                        <div className="para">
+                            <p>  <FcApproval /> Last Updated
+                                -
+                                February 22, 2020</p>
+                            <p><IoIosInformationCircle />Advertising Disclosure</p>
+                        </div>
+                        <div className="para2">
+                            Top Relevant<IoIosArrowDown />
+                        </div>
+                    </div>
+                    <div className="content2">
+                        <p>Tools</p>
+                        <p>AWS Builder</p>
+                        <p>Start Build</p>
+                        <p>Build Supplies</p>
+                        <p>Tooling</p>
+                        <p>BlueHosting</p>
+
+                    </div>
+                    <div className="content3">
+                        <p>Home</p> <IoIosArrowForward />
+                        <p>Hosting for all</p><IoIosArrowForward />
+                        <p>Hosting</p><IoIosArrowForward />
+                        <p>Hosting6</p><IoIosArrowForward />
+                        <p>Hosting5</p><IoIosArrowForward />
+                    </div>
+                    <div className="content4">
+                        <img src="https://s3-alpha-sig.figma.com/img/3250/2fc6/187db358671328189d3da83955a6ffe9?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ax2xncFBKe1xE2EsxdvElceh8oIqKj5N0jH4NJTiBcahukOzPxx4h1z6yHWL5iAGwcZziaBWfUhJnOIkYfeTkuKTAQ3cZ0jcc19S7zOzX3YtV8qd2foEnwflcvKEK8-iqw~VegstF5Nw8QROw9i36ihQaXHBsSdmfGb7V~XdDUKn2fabEOEcm2tN9ETF0zbCn6Tq~PYk9pxyghi5P3ugATGWGP8ljAH8tlJzjgbGXnUBNNTS4sfQDZNsmBvFmdtbDb28LeFrh~MRRFxi7hGf1M9mBH8gyb7qPYFk0pRsrwIdNrDPTsn0IaF8FPwAR8zL3wCBUlfMgzxP1E-gqRbjxQ__" />
+                        <p className="builder">Builder 1</p>
+                        <p className="best"><IoLogoCodepen />Best Choice</p>
+
+
+                        <div>
+                            <p><span>WixPro 72-Inch Responsive Website Builder</span>- Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)</p>
+                            <p className="flexstart">Main highlights
+                                <br></br>
+
+                                [What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.</p>
+                            <p>See More <IoIosArrowDown /></p>
+                            <p className="circle">1</p>
+                        </div>
+                        <div>
+                            <div className="box1">
+                                <p>9.8</p>
+                                <p>Exceptional</p>
+                                <p className="star"><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStarHalf /></p>
+                            </div>
+                            <button>View</button>
+                        </div>
+
+
+                    </div>
+
+                    <div className="content4">
+                        <img src="https://s3-alpha-sig.figma.com/img/3250/2fc6/187db358671328189d3da83955a6ffe9?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ax2xncFBKe1xE2EsxdvElceh8oIqKj5N0jH4NJTiBcahukOzPxx4h1z6yHWL5iAGwcZziaBWfUhJnOIkYfeTkuKTAQ3cZ0jcc19S7zOzX3YtV8qd2foEnwflcvKEK8-iqw~VegstF5Nw8QROw9i36ihQaXHBsSdmfGb7V~XdDUKn2fabEOEcm2tN9ETF0zbCn6Tq~PYk9pxyghi5P3ugATGWGP8ljAH8tlJzjgbGXnUBNNTS4sfQDZNsmBvFmdtbDb28LeFrh~MRRFxi7hGf1M9mBH8gyb7qPYFk0pRsrwIdNrDPTsn0IaF8FPwAR8zL3wCBUlfMgzxP1E-gqRbjxQ__" />
+                        <p className="builder">Builder </p>
+                        <p className="best"><IoLogoCodepen />Best value</p>
+
+
+                        <div>
+                            <p><span>SiteCraft 68-Inch Ultimate Web Design Studio</span>-  Advanced Site Creation Toolkit, Intuitive Drag-and-Drop Editor for Dynamic Websites and E-commerce Platforms (Green/White)</p>
+                            <p className="flexstart">Main highlights
+                                <br></br>
+                                [What You Get]: Gain access to the SiteCraft design studio, featuring a robust selection of design elements, SEO optimization tools, and e-commerce integration.</p>
+                            <p className="see">See More <IoIosArrowDown /></p>
+                            <p className="circle">2</p>
+                        </div>
+                        <div>
+                            <div className="box1">
+                                <p>9.5</p>
+                                <p>Excellent</p>
+                                <p className="star"><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStarHalf /></p>
+                            </div>
+                            <button>View</button>
+                        </div>
+
+
+                    </div>
+
+                    <div className="content4">
+                        <img src="https://s3-alpha-sig.figma.com/img/3250/2fc6/187db358671328189d3da83955a6ffe9?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ax2xncFBKe1xE2EsxdvElceh8oIqKj5N0jH4NJTiBcahukOzPxx4h1z6yHWL5iAGwcZziaBWfUhJnOIkYfeTkuKTAQ3cZ0jcc19S7zOzX3YtV8qd2foEnwflcvKEK8-iqw~VegstF5Nw8QROw9i36ihQaXHBsSdmfGb7V~XdDUKn2fabEOEcm2tN9ETF0zbCn6Tq~PYk9pxyghi5P3ugATGWGP8ljAH8tlJzjgbGXnUBNNTS4sfQDZNsmBvFmdtbDb28LeFrh~MRRFxi7hGf1M9mBH8gyb7qPYFk0pRsrwIdNrDPTsn0IaF8FPwAR8zL3wCBUlfMgzxP1E-gqRbjxQ__" />
+                        <p className="builder">Builder 2</p>
+
+
+
+                        <div>
+                            <p><span>WixPro 72-Inch Responsive Website Builder</span>- Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)</p>
+                            <p className="flexstart">Main highlights
+                                <br></br>
+                                [What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.</p>
+                            <p className="see">See More <IoIosArrowDown /></p>
+                            <p className="circle">3</p>
+                        </div>
+                        <div>
+                            <div className="box1">
+                                <p>9.3</p>
+                                <p>Exceptional</p>
+                                <p className="star"><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStarHalf /></p>
+                            </div>
+                            <button>View</button>
+                        </div>
+
+
+                    </div>
+
+
+                    <div className="content4">
+                        <img src="https://s3-alpha-sig.figma.com/img/3250/2fc6/187db358671328189d3da83955a6ffe9?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ax2xncFBKe1xE2EsxdvElceh8oIqKj5N0jH4NJTiBcahukOzPxx4h1z6yHWL5iAGwcZziaBWfUhJnOIkYfeTkuKTAQ3cZ0jcc19S7zOzX3YtV8qd2foEnwflcvKEK8-iqw~VegstF5Nw8QROw9i36ihQaXHBsSdmfGb7V~XdDUKn2fabEOEcm2tN9ETF0zbCn6Tq~PYk9pxyghi5P3ugATGWGP8ljAH8tlJzjgbGXnUBNNTS4sfQDZNsmBvFmdtbDb28LeFrh~MRRFxi7hGf1M9mBH8gyb7qPYFk0pRsrwIdNrDPTsn0IaF8FPwAR8zL3wCBUlfMgzxP1E-gqRbjxQ__" />
+                        <p className="builder1">CDK</p>
+
+
+
+                        <div>
+                            <p><span>CDK Resposive Builder:</span>- An extensive library of widgets and apps, and detailed step-by-step guides</p>
+                            <p className="off">26% Off</p>
+                            <p className="flexstart">Main highlights </p>
+                            <div className="flex1">
+                                <p> <span className="cool">9.9</span>building responsive</p>
+                                <p> <span className="cool">8.9</span>cool</p>
+                                <p> <span className="cool">8.9</span>Docs</p>
+                            </div>
+                            <div>
+                                <p>Why We loved it.</p>
+                                <p> <IoIosCheckmarkCircleOutline />Documentation</p>
+                                <p> <IoIosCheckmarkCircleOutline />Easy Use</p>
+                                <p> <IoIosCheckmarkCircleOutline />Out of box</p>
+                            </div>
+
+                            <p className="see">See More <IoIosArrowDown /></p>
+                            <p className="circle">4</p>
+                        </div>
+                        <div>
+                            <div className="box1">
+                                <p>9.1</p>
+                                <p>Very Good</p>
+                                <p className="star"><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStarHalf /></p>
+                            </div>
+                            <button className="btn1">View</button>
+                        </div>
+
+
+                    </div>
+
+                    <div className="content5">
+                        <h3>Related deals you might like for</h3>
+                        <div className="maincontent">
+                            <div className="content5-box">
+                                <img src="https://s3-alpha-sig.figma.com/img/3250/2fc6/187db358671328189d3da83955a6ffe9?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ax2xncFBKe1xE2EsxdvElceh8oIqKj5N0jH4NJTiBcahukOzPxx4h1z6yHWL5iAGwcZziaBWfUhJnOIkYfeTkuKTAQ3cZ0jcc19S7zOzX3YtV8qd2foEnwflcvKEK8-iqw~VegstF5Nw8QROw9i36ihQaXHBsSdmfGb7V~XdDUKn2fabEOEcm2tN9ETF0zbCn6Tq~PYk9pxyghi5P3ugATGWGP8ljAH8tlJzjgbGXnUBNNTS4sfQDZNsmBvFmdtbDb28LeFrh~MRRFxi7hGf1M9mBH8gyb7qPYFk0pRsrwIdNrDPTsn0IaF8FPwAR8zL3wCBUlfMgzxP1E-gqRbjxQ__" />
+                                <br></br>
+                                <div className="content5-box2">
+                                    <p className="off">20% off</p><span>Limited Time</span>
+                                    <p>Webbuilder 1</p>
+                                    <p>Computer  Modern clasic with wix support</p>
+                                    <p>$39.96</p> <span className="small">$49.96</span> <span className="red">(20% Off)</span>
+                                    <button>view </button>
+                                </div>
+                            </div >
+                            <div className="content5-box">
+                                <img src="https://s3-alpha-sig.figma.com/img/3250/2fc6/187db358671328189d3da83955a6ffe9?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ax2xncFBKe1xE2EsxdvElceh8oIqKj5N0jH4NJTiBcahukOzPxx4h1z6yHWL5iAGwcZziaBWfUhJnOIkYfeTkuKTAQ3cZ0jcc19S7zOzX3YtV8qd2foEnwflcvKEK8-iqw~VegstF5Nw8QROw9i36ihQaXHBsSdmfGb7V~XdDUKn2fabEOEcm2tN9ETF0zbCn6Tq~PYk9pxyghi5P3ugATGWGP8ljAH8tlJzjgbGXnUBNNTS4sfQDZNsmBvFmdtbDb28LeFrh~MRRFxi7hGf1M9mBH8gyb7qPYFk0pRsrwIdNrDPTsn0IaF8FPwAR8zL3wCBUlfMgzxP1E-gqRbjxQ__" />
+                                <br></br>
+                                <div className="content5-box2">
+                                    <p className="off">20% off</p><span>Limited Time</span>
+                                    <p>Webbuilder 1</p>
+                                    <p>Computer  Modern clasic with wix support</p>
+                                    <p>$39.96</p> <span className="small">$49.96</span> <span className="red">(20% Off)</span>
+                                    <button>view</button>
+                                </div>
+                            </div >
+                            <div className="content5-box">
+                                <img src="https://s3-alpha-sig.figma.com/img/3250/2fc6/187db358671328189d3da83955a6ffe9?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ax2xncFBKe1xE2EsxdvElceh8oIqKj5N0jH4NJTiBcahukOzPxx4h1z6yHWL5iAGwcZziaBWfUhJnOIkYfeTkuKTAQ3cZ0jcc19S7zOzX3YtV8qd2foEnwflcvKEK8-iqw~VegstF5Nw8QROw9i36ihQaXHBsSdmfGb7V~XdDUKn2fabEOEcm2tN9ETF0zbCn6Tq~PYk9pxyghi5P3ugATGWGP8ljAH8tlJzjgbGXnUBNNTS4sfQDZNsmBvFmdtbDb28LeFrh~MRRFxi7hGf1M9mBH8gyb7qPYFk0pRsrwIdNrDPTsn0IaF8FPwAR8zL3wCBUlfMgzxP1E-gqRbjxQ__" />
+                                <br></br>
+                                <div className="content5-box2">
+                                    <p className="off">20% off</p><span>Limited Time</span>
+                                    <p>Webbuilder 1</p>
+                                    <p>Computer  Modern clasic with wix support</p>
+                                    <p>$39.96</p> <span className="small">$49.96</span> <span className="red">(20% Off)</span>
+                                    <button>view</button>
+                                </div>
+                            </div >
+
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+                <div className="content6">
+                    <p>Sign up and get exclusive special deals</p>
+
+                    <button className="btn2">Sign Up</button>
+                </div>
+
+                <div className="footer">
+                    <div className="footer-content1">
+                        <h4>Categories</h4>
+                        <p>Web Builder</p>
+                        <p>Hosting</p>
+                        <p>Data Center</p>
+                        <p>Robotic-Automation</p>
+                    </div>
+                    <div className="footer-content2">
+                        <h4>Contact</h4>
+                        <p>Contact</p>
+                        <p>Privacy Policy</p>
+                        <p>Terms Of Service</p>
+                        <p>Categories</p>
+                        <p>About</p>
+
+                    </div>
+                    <div className="footer-content3">
+                    United States<IoIosArrowDown />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default App;
