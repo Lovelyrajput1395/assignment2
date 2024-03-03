20e0753e42556b2739127927e9fc35e2961253c7
lovely <lovelyrajput1395@gmail.com>
2024-03-03 20:05:25 +0530

    added assignment files

--- src/helo.js
+++
@@ -1,8 +0,0 @@
-function Helo(){
-    return(
-        <div>
-            <h5>hello </h5>
-        </div>
-    )
-}
-export default Helo;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-// import App from "./fire base.js";
-// import ReactDOM from "react-dom";
-// ReactDOM.render(<App/>,document.getElementById("root"))
-// import {Subodh} from "./subodh";
-// import  ReactDOM from "react-dom";
-// ReactDOM .render(<Subodh/>,document.getElementById("root"))
-
-import App from "./classcomponemt";
+import App from "./assignment.js";
 import ReactDOM from "react-dom";
 ReactDOM.render(<App/>,document.getElementById("root"))
